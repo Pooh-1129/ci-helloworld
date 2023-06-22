d5c51d2c42c9c0288434a1ad0f3c0ede11aeda5f
Ali Darvish <darvishdarab@gmail.com>
2023-06-21 23:05:05 -0400

    Updated Dockerfile

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -51,7 +51,7 @@ public class ArrayUtilsTest {
     // The reason is that error states are not repairable by subsequent processing.
     @Test
     public void testOddOrPositiveBothPositivesAndNegatives() {
-        assertEquals(2, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
 
 
